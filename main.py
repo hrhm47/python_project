99a5a7c87c44d7d67fc2cc4aa691e76ae82d7d7c
hrhm47 <haseebr470@gmail.com>
2021-09-03 08:28:38 +0500

    gussingNumber + Madlibs Game

--- main.py
+++ main.py
@@ -38,3 +38,19 @@ def guess_number(rng):
 
 rng = int(input("\t\t Enter a range to Guess a Number from Computer: "))
 guess_number(rng)
+
+
+
+print("\n\n\t\t-------------------------Madlibs Game--------------------------------\n\n")
+
+noun=input("Enter the name of person:")
+fruit_name=input("Enter the name of fruit:")
+color=input("Enter the color of fruit:")
+
+print("\n\n")
+
+story="Yesterday, we visited in " + str(noun) + "'s orchard. We saw different colors of " + str(fruit_name) +"s." \
+"I ate ",color, fruit_name+ "'s. They were so delicious."
+
+
+print(story)
